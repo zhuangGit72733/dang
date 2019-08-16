1de388219ce4e3f0135410c0a4139f8011adb4da
zhuanggit <1437383705@qq.com>
2019-08-16 11:05:35 +0800

    video拆分

--- controllers/videos.go
+++ controllers/videos.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"github.com/astaxie/beego"
 	"github.com/astaxie/beego/logs"
+	"github.com/astaxie/beego/orm"
 )
 
 type VideosController struct {
@@ -13,17 +14,20 @@ type VideosController struct {
 }
 
 func (c *VideosController) Video() {
-	url, _ := config.InitConfig()
 	data, err := models.GetVideos()
 	if err != nil {
 		logs.Debug("videos controller err:%v", err)
 	}
+	c.Data["json"] = addPath(data)
+	c.ServeJSON()
+}
+func addPath(data []orm.Params) []orm.Params {
+	url, _ := config.InitConfig()
 	for k, v := range data {
 		data[k]["video"] = fmt.Sprintf("%v/uploads/videos/%v", url, v["video"])
 		if data[k]["img"] != nil {
 			data[k]["img"] = fmt.Sprintf("%v/uploads/activity/%v", url, v["img"])
 		}
 	}
-	c.Data["json"] = data
-	c.ServeJSON()
+	return data
 }
